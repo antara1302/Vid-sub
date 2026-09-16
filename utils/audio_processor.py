13433b75510d1fbff0d705ca411d7b58d6523722
antara1302 <antara.desh13@gmail.com>
2026-09-16 17:52:36 +0530

    Fix bgutil script provider path

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -34,14 +34,12 @@ def download_youtube_audio(url :str) ->str:
         "plugin_dirs": [PLUGIN_DIR],
         "extractor_args": {
             "youtubepot-bgutilscript": {
-                "script_path": os.path.abspath(
+                "server_home": os.path.abspath(
                     os.path.join(
                         os.path.dirname(__file__),
                         "..",
                         "bgutil-ytdlp-pot-provider",
                         "server",
-                        "build",
-                        "generate_once.js",
                     )
                 )
             }
