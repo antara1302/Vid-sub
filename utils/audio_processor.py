13fc5c65058df92742ea683d2c3b2d681de0202b
antara1302 <antara.desh13@gmail.com>
2026-09-16 23:17:54 +0530

    Fix bgutil youtube player client

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -45,7 +45,11 @@ def download_youtube_audio(url :str) ->str:
         "plugin_dirs": [PLUGIN_DIR],
         "extractor_args": {
             "youtubepot-bgutilhttp": {
-                "base_url": "http://127.0.0.1:4416"
+                "base_url": "http://127.0.0.1:4416",
+                "disable_innertube": "1"
+            },
+            "youtube": {
+                "player_client": ["mweb"]
             }
         },
         "http_headers": {
